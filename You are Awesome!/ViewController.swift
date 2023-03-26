afb1b0cfbb7fd20e8b18f3292d6764886e229a6f
YHYen <cloudyhyen@gmail.com>
2023-03-26 22:25:09 +0800

    Section 2.15 Add sound player

--- You are Awesome!/ViewController.swift
+++ You are Awesome!/ViewController.swift
@@ -6,12 +6,14 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
     
     var messageIndex = -1
+    var audioPlayer: AVAudioPlayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,21 +25,40 @@ class ViewController: UIViewController {
     @IBAction func messageButtonPressed(_ sender: UIButton) {
         // print("The message button was pressed! 😎")
         
+        // set messages array
         let messages = ["First Message", "You Are Awesome!", "You Are Great, I need more message", "You Are Da Bomb", "Last Message"]
+        
+        // set messageIndex to traversal the messages array
         var newMessageIndex: Int
         
+        // choose random message and use repeat(do-while) to avoid the same message show again
         repeat {
             newMessageIndex = Int.random(in: 0...messages.count - 1)
         } while newMessageIndex == messageIndex
         
+        // set newMessageIndex to messageIndex
         messageIndex = newMessageIndex
+        
+        // set message on messageLabel
         messageLabel.text = messages[messageIndex]
         
-        
-        
-        
-        
+        // set image on imageView
         imageView.image = UIImage(named: "bird")
+        
+        // set sound when button clicked
+        // set sound data from assets package
+        if let sound = NSDataAsset(name: "sound0") {
+            // if not nil, create sound
+            do {
+                try audioPlayer = AVAudioPlayer(data: sound.data)
+                audioPlayer.play()
+            } catch {
+                print("ERROR: \(error.localizedDescription) Could not initialize AVAudioPlayer object.")
+            }
+        } else {
+            // if nil print error message, could not read data from file sound0
+            print("ERROR: file \("sound0") did not load.")
+        }
     }
     
     
